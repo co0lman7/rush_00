2b708b335236f21c298972ef9ec708ac3a60131e
co0lman7 <hajdariarlind74@gmail.com>
2025-07-26 03:11:28 +0200

    Solution to Ex1 of Rush 0

---
+++ rush_01.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush_01.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arlind.hajdari <arlind.hajdari@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 23:32:06 by arlind.hajd       #+#    #+#             */
+/*   Updated: 2025/07/26 03:10:33 by arlind.hajd      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/***\  
+*   *  
+\***/  
+
+/*
+(1,1)(1,2)(1,3)(1,4)
+(2,1)(2,2)(2,3)(2,4)
+(3,1)(3,2)(2,3)(3,4)
+*/
+
+#include <unistd.h>
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void rush01(int x, int y)
+{
+    int col;
+    col=1;
+
+    int row;
+
+    if(x<1 || y<1)
+    {
+        return;
+    }
+
+    else{
+        while (col<=y)
+        {
+            row=1;
+            while(row<=x)
+            {
+            if((row==1 && col==1) || (row==x && col==y) && (!(row ==1 && col == y)))
+                {
+                    ft_putchar('/');
+
+                }
+
+            else if ((row==1 && col==y)|| (row==x && col==1) || (row ==x && col == y))
+            {
+                ft_putchar('\\');
+            }
+            else if((col==1 || col==y) || (row==1 || row==x))
+            {
+                ft_putchar('*');
+            }
+            else
+            {
+                ft_putchar(' ');
+            }
+            row++;
+            }
+            ft_putchar('\n');
+            col++;
+        }
+    }
+}
+int main()
+{
+    rush01(10,15);
+    write(1, "\n", 1);
+    rush01(5,1);
+    write(1, "\n", 1);
+    rush01(1,2);
+    write(1, "\n", 1);
+    rush01(1,5);
+    write(1, "\n", 1);
+}
